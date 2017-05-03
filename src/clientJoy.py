f5a493221abc0a212bdc8cb4f96aee1996c6bd9c
Vinícius Campos <viniciuscampos120@gmail.com>
2017-05-03 00:07:43 -0300

    Fix ifelse statement

--- src/clientJoy.py
+++ src/clientJoy.py
@@ -39,7 +39,6 @@ HOST = '127.0.0.1'
 PORT = 4325
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
-try
 tcp.connect(dest)
 msg = ''
 while msg <> '\x18':
@@ -54,8 +53,7 @@ while msg <> '\x18':
             msg = 's 1 0'
         elif bye:
             msg = 's 0 1'
-        else
+        else:
             msg = 's 0 0'
     tcp.send(msg)
 tcp.close()
-
